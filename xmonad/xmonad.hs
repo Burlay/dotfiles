ad2b8b46a79fa1ed04c09c24b50bf205f4bd4d4b
Bastiaan Rasing <bastiaan@rasing.me>
2012-02-25 20:48:31 +0100

    Added basic xmonad config

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,24 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Layout.NoBorders
+import XMonad.Hooks.SetWMName
+
+myManageHook = composeAll
+	[ className =? "net-minecraft-LauncherFrame" --> doShift "4"
+	, className =? "net-minecraft-LauncherFrame" --> doFloat ]
+
+myModMask = mod4Mask
+myBorderWidth = 1
+myTerminal = "urxvt"
+myBar = "xmobar"
+
+myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+myConfig = defaultConfig { modMask = myModMask 
+			 , terminal = myTerminal
+			 , borderWidth = myBorderWidth
+			 , manageHook = myManageHook <+> manageHook defaultConfig
+			 }
