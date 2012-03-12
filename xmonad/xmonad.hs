cad9401b9fb982917d7f74f8cfb6814e13359eb5
Bastiaan Rasing <bastiaan@rasing.me>
2012-03-12 20:37:39 +0100

    Disabled the annoying mouse follow

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -4,21 +4,23 @@ import XMonad.Layout.NoBorders
 import XMonad.Hooks.SetWMName
 
 myManageHook = composeAll
-	[ className =? "net-minecraft-LauncherFrame" --> doShift "4"
-	, className =? "net-minecraft-LauncherFrame" --> doFloat ]
+  [ className =? "net-minecraft-LauncherFrame" --> doShift "4"
+  , className =? "net-minecraft-LauncherFrame" --> doFloat ]
 
 myModMask = mod4Mask
 myBorderWidth = 1
 myTerminal = "urxvt"
 myBar = "xmobar"
+myFocusFollowsMouse = False
 
 myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
 
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
-myConfig = defaultConfig { modMask = myModMask 
-			 , terminal = myTerminal
-			 , borderWidth = myBorderWidth
-			 , manageHook = myManageHook <+> manageHook defaultConfig
-			 }
+myConfig = defaultConfig { modMask = myModMask
+       , terminal = myTerminal
+       , borderWidth = myBorderWidth
+       , manageHook = myManageHook <+> manageHook defaultConfig
+       , focusFollowsMouse = myFocusFollowsMouse
+       }
