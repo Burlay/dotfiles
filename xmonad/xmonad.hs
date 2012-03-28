873e2482ebb017fd9cc54d9f4cd24c0697902f28
Bastiaan Rasing <bastiaan@rasing.me>
2012-03-28 09:43:30 +0200

    removed mirror tall layout, added smartborders to the layouthook

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -12,6 +12,19 @@ myBorderWidth = 1
 myTerminal = "urxvt"
 myBar = "xmobar"
 myFocusFollowsMouse = False
+myLayoutHook = smartBorders $ tiled ||| Full
+  where
+    -- default tiling algorithm partitions the screen
+    tiled = Tall nmaster delta ratio
+
+    -- The default number of windows in the master pane
+    nmaster = 1
+
+    -- Default proportion of screen occupied by master
+    ratio   = 1/2
+
+    -- Percent of screen to increment by when resizing
+    delta   = 3/100
 
 myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
 
@@ -23,4 +36,6 @@ myConfig = defaultConfig { modMask = myModMask
        , borderWidth = myBorderWidth
        , manageHook = myManageHook <+> manageHook defaultConfig
        , focusFollowsMouse = myFocusFollowsMouse
+       , startupHook = setWMName "LG3D"
+       , layoutHook = myLayoutHook
        }
